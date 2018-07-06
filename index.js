75a5d16a2ba12c3e5204fe48743bd247cea92011
thabtom <tsehayegh@gmail.com>
2018-07-06 12:17:27 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+//Toggle classes for responsiveness
+function toggleResponsiveClass() {
+  	$('#myTopnav').toggleClass('responsive');
+}
+
+//display bio;
+function displayBio(){
+	$('.nav-bio').on('click', function(event) {
+		event.preventDefault();
+		showHideSections('.bio', '.about, .projects');
+	});
+}
+$(displayBio);
+
+function displayProjects(){
+	$('.nav-projects').on('click', function(event) {
+		event.preventDefault();
+		showHideSections('.projects', '.bio, .about');
+	});
+}
+$(displayProjects);
+
+function displayAbout(){
+	$('.nav-about').on('click', function(event) {
+		event.preventDefault();
+		showHideSections('.about', '.bio, .projects');
+
+	})
+}
+$(displayAbout);
+
+
+
+
+
+function showHideSections(sectionToShow, sectionToHide){
+	$(sectionToShow).removeClass("hidden");
+	$(sectionToHide).addClass("hidden");
+	$('.icon').click();
+} 
